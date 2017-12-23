11d72d13443913301b8646ff04fdb0c59f6626ae
jw-green <repo.jwgreen@gmail.com>
2017-12-23 16:40:09 +0000

    Genesis

---
+++ README.md
@@ -0,0 +1,51 @@
+# Word Cloud Tool
+
+Uses the package `wordcloud` to generate WordClouds.
+
+## Structure
+
+```plaintext
+.
+|-- README.md
+|-- mask.py
+`-- ref
+    |-- input_images
+    |-- srcTxt
+    `-- typefaces
+        |
+        `-- Raleway-Light.ttf
+```
+
+## Installation Instructions
+
+It may be preferable to create a new virtual environment to avoid modifying global python packages:
+
+```plaintext
+virtualenv [app-name]
+```
+
+The script also requires `scipy` and `wordcloud`.
+
+```plaintext
+pip install scipy
+```
+
+```plaintext
+pip install wordcloud
+```
+
+## Usage
+
+On run, the script will present a few mandatory fields:
+
+1. Input File Name (Text)
+1. Image File Name (Image)
+1. Output File Name (Image)
+
+As suggested by the folder names, any images that are intended for use as the word cloud background should go in the `ref/input_images` folder.
+
+Text files (in .txt format) should be placed into the `ref/srcTxt` folder.
+
+Any additional typefaces can be placed in the `ref/typefaces` folder, but for use, must be amended in the main script.
+
+## Background
---
+++ wordCloud.py
@@ -0,0 +1,60 @@
+from scipy.misc import imread
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+
+def openFile(filename):
+    text = open(filename, encoding='utf-8').read()
+    return text
+
+
+def openImage(image_filename):
+    image = imread(image_filename)
+    return image
+
+
+def setImageColour(image_handle):
+    image_colours = ImageColorGenerator(bg_image)
+    return image_colours
+
+
+def createWordCloud(font_path, image, stopwords, text):
+    wordCloud = WordCloud(font_path=font_path, background_color="white",
+                          max_words=2000, mask=image, stopwords=stopwords)
+
+    wordCloud.generate(text)
+
+    wordCloud.recolor(color_func=setImageColour(bg_image))
+
+    return wordCloud
+
+
+def saveWordCloud(wordCloud, filename):
+    wordCloud.to_file(filename)
+
+
+def getValidInput( input_string ):
+
+    user_input = input( input_string )
+
+    if "." not in user_input:
+        print("[ERROR] Please enter a valid filename and extension.")
+        getValidInput( input_string )
+    else:
+        return user_input
+
+stopwords = set(STOPWORDS)
+
+filename = getValidInput("Text File Name: ")
+text = openFile('ref/srcTxt/' + filename)
+
+image_filename = getValidInput("Image File Name: ")
+bg_image = openImage("ref/input_images/" + image_filename)
+
+image_colours = setImageColour(bg_image)
+
+wordCloud = createWordCloud(
+    "ref/typefaces/Raleway-Light.ttf", bg_image, stopwords, text)
+
+output_filename = getValidInput("Output File Name: ")
+saveWordCloud(wordCloud, output_filename)
